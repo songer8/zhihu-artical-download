aacee7f96ea70a63a9b577dbb025a471d224aaa9
songer <15921028245@163.com>
2020-06-01 20:45:33 +0800

    增加单篇文章下载功能

--- background.js
+++ background.js
@@ -1,11 +1,27 @@
 chrome.runtime.onMessage.addListener(
     function (request,sender, sendResponse) {
-        if (request.contentScriptQuery == 'fetchDownload') {
-            var token = request.token;
-            var articleApi = "https://www.zhihu.com/api/v4/members/"+token+"/articles?include=data[*].content&offset=0&limit=1000&sort_by=created";
+        if (request.contentScriptQuery == 'fetchDownloadAll') {
+            var articleApi = request.articleApi;           
             fetch(articleApi)
                 .then(response => response.json())
                 .then(result => sendResponse(result));
+            return true;
+        }
+    }
+);
+
+
+chrome.runtime.onMessage.addListener(
+    function (request,sender, sendResponse) {
+        if (request.contentScriptQuery == 'fetchDownload') {
+            var singleArticalUrl = request.singleArticalUrl;
+            fetch(singleArticalUrl)
+                .then(function(response){
+                    return response.json();
+                })
+                .then(function(result){
+                    sendResponse(result);
+                });
             return true;
         }
     }
--- zhihu.js
+++ zhihu.js
@@ -1,22 +1,21 @@
-//add button in the page
+//add button in the page;
 let btnElement = document.createElement("button");
 let wrapper = document.querySelector(".List-headerOptions");
 let writeArtical = document.querySelector(".Profile-writePosts");
 if (wrapper.contains(writeArtical)) {
-    wrapper.appendChild(btnElement);
-    btnElement.id = "uDownload";
     btnElement.className = "uDownload";
-    document.getElementById("uDownload").innerText = "下载全部文章";
+    btnElement.innerText = "下载全部文章";
+    wrapper.appendChild(btnElement);
 }
 
 
-//obtain userID
-let message = document.getElementById("ProfileHeader").getAttribute("data-za-extra-module");
-let token = JSON.parse(message).card.content.token;
+//obtain userID;
+let token = window.location.href.split("/")[4];
+var articleApi = "https://www.zhihu.com/api/v4/members/" + token + "/articles?include=data[*].content&offset=0&limit=1000&sort_by=created";
 
 function uDownloadAll() {
     chrome.runtime.sendMessage(
-        { contentScriptQuery: 'fetchDownload', token: token },
+        { contentScriptQuery: 'fetchDownloadAll', articleApi: articleApi },
         result => {
             result.data.forEach((article, index) => {
                 // html to markdown
@@ -25,7 +24,9 @@ function uDownloadAll() {
 
                 var link = document.createElement('a');
                 //设置下载的文件名
-                link.download = `article-${index}.md`;
+                var turndownFilename = new TurndownService();
+                let filename = turndownFilename.turndown(article.title);
+                link.download = `article-${filename}.md`;
                 //设置下载内容
                 link.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(markdown);
                 link.innerText = '下载';
@@ -38,3 +39,48 @@ function uDownloadAll() {
 }
 
 btnElement.addEventListener("click", uDownloadAll);
+
+let contentItems = document.querySelectorAll('.ContentItem.ArticleItem');
+contentItems.forEach(function (contentItem, index) {
+    //add buttom;
+    let downloadBtn = document.createElement('button');
+    downloadBtn.className = 'downloadItem';
+    downloadBtn.innerHTML = '下载此文章';
+    contentItem.appendChild(downloadBtn);
+
+    //obtain single singleArticalUrl;
+    let pageId = JSON.parse(contentItem.getAttribute('data-zop')).itemId;
+    console.log(pageId);
+    let singleArticalUrl = `https://zhuanlan.zhihu.com/api/articles/${pageId}`;
+
+
+    function uDownloadSingle() {
+        chrome.runtime.sendMessage(
+            { contentScriptQuery: 'fetchDownload', singleArticalUrl: singleArticalUrl },
+            function (result) {
+                // html to markdown
+                var turndownService = new TurndownService();
+                var markdown = turndownService.turndown(result.content);
+
+                var link = document.createElement('a');
+                //设置下载的文件名
+                var turndownFilename = new TurndownService();
+                let filename = turndownFilename.turndown(result.title);
+                link.download = `article-${filename}.md`;
+                //设置下载内容
+                link.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(markdown);
+                link.innerText = '下载';
+                link.style.display = 'block';
+                document.body.appendChild(link);
+                link.click();
+            }
+        );
+    }
+    downloadBtn.addEventListener('click', uDownloadSingle);
+
+});
+
+
+
+
+
